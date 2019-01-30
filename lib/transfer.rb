9acb687839b31ff220378897f54831e722ea9c7a
David Charles <ratlover03@gmail.com>
2019-01-30 14:34:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,4 +25,12 @@ end
     end
  end
 
+ def reverse_transfer
+    if @status == "complete" && @receiver.balance >= @amount && @receiver.status == "open"
+      @sender.balance = @sender.balance + @amount
+      @receiver.balance = @receiver.balance - @amount
+      @status = "reversed"
+    end
+  end
+
 end
