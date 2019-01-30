70bddd2e2d49ab97fdfc141e739ed7e3e807b89d
David Charles <ratlover03@gmail.com>
2019-01-30 14:38:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,15 +15,13 @@ def valid?
   end
 end
  def execute_transaction
-   if @status == "complete" && @sender.balance >= @amount && @sender.status == "open"
-     @sender.balance -= @amount
-     @receiver.balance += @amount
-     @status == "complete"
-   else
-     @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
- end
+   
+     if @status == "pending"
+  @sender.balance -= @amount
+  @receiver.balance += @amount
+  @status = "complete"
+end
+end
 
  def reverse_transfer
     if @status == "complete" && @receiver.balance >= @amount && @receiver.status == "open"
